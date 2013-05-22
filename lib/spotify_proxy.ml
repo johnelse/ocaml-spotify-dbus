511c72e4919bb33bd7df8dc562de4fa2bba09d91
John Else <john.else@citrix.com>
2013-05-22 01:53:36 +0100

    Rename proxy_of_bus to of_bus
    Signed-off-by: John Else <john.else@citrix.com>

--- lib/spotify_proxy.ml
+++ lib/spotify_proxy.ml
@@ -1,4 +1,4 @@
-let proxy_of_bus bus =
+let of_bus bus =
   OBus_proxy.make
     (OBus_peer.make bus "org.mpris.MediaPlayer2.spotify")
     ["org"; "mpris"; "MediaPlayer2"]
--- lib/spotify_proxy.mli
+++ lib/spotify_proxy.mli
@@ -1,1 +1,1 @@
-val proxy_of_bus: OBus_bus.t -> OBus_proxy.t
+val of_bus: OBus_bus.t -> OBus_proxy.t
